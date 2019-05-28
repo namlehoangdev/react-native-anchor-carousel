ef9397175d37b4c8d8ad5f3179cc417014dbc566
Le Hoang Nam <lehoangnamm1997@gmail.com>
2019-05-28 16:50:01 +0700

    add npm ignore

--- examples/index.js
+++
@@ -1,7 +0,0 @@
-/** @format */
-
-import {AppRegistry} from 'react-native';
-import App, {App2} from './App';
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => App);

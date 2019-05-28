4995c62069d4eee2a310a994dcb11265b4e43630
Le Hoang Nam <lehoangnamm1997@gmail.com>
2019-05-28 15:46:09 +0700

    add examples.

---
+++ examples/App.js
@@ -0,0 +1,69 @@
+import React, {Component} from 'react';
+import {StyleSheet, Text, TouchableOpacity, View, Dimensions} from 'react-native';
+import Carousel from 'react-native-anchor-carousel';
+
+const {width} = Dimensions.get('window');
+
+const data = [
+    {backgroundColor: 'red'},
+    {backgroundColor: 'green'},
+    {backgroundColor: 'blue'},
+    {backgroundColor: 'yellow'},
+];
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'black',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    carousel: {
+        height: 200,
+        borderWidth: 5,
+        borderColor: 'white',
+        backgroundColor: 'orange'
+    },
+    item: {
+        borderWidth: 2,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    text: {
+        fontSize: 100,
+        fontWeight: 'bold'
+    }
+});
+
+export default class App extends Component {
+    renderItem = ({item, index}) => {
+        const {backgroundColor} = item;
+        return (
+            <TouchableOpacity style={[styles.item, {backgroundColor}]}
+                              onPress={() => {
+                                  this._carousel.scrollToIndex(index);
+                              }}>
+                <Text style={styles.text}>
+                    {index.toString()}
+                </Text>
+            </TouchableOpacity>)
+    };
+
+    render() {
+        return (<View style={styles.container}>
+            <Carousel style={styles.carousel}
+                      data={data}
+                      renderItem={this.renderItem}
+                      itemWidth={200}
+                      containerWidth={width - 20}
+                        //pagingEnable={false}
+                      separatorWidth={20}
+                      ref={(c) => {
+                          this._carousel = c;
+                      }}
+            />
+        </View>)
+    }
+}
+
+
---
+++ examples/index.js
@@ -0,0 +1,7 @@
+/** @format */
+
+import {AppRegistry} from 'react-native';
+import App, {App2} from './App';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
